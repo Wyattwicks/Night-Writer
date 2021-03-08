fa2d6c63705b9f57adfe68830d5bdd04fb37e424
Wyattwicks <wyattwicks@gmail.com>
2021-03-07 22:54:35 -0700

    Get iteration 2 to work

--- lib/night_writer.rb
+++ lib/night_writer.rb
@@ -7,16 +7,16 @@ class NightWriter
   attr_reader :file_reader, :file_writer, :translator, :braille_translation
 
   def initialize
-    @file_reader = FileReader.new
-    @file_writer = FileWriter.new
+
+    @file_reader = FileReader.new.read
+
+
+
   end
 
   def display_confirmation
-    if @file_writer.write_new_file
+
       puts "Created '#{ARGV[1]}' containing xxxxx characters."
-    else
-      puts "File not read"
-    end
   end
 
 
@@ -25,15 +25,15 @@ class NightWriter
     @braille_translation = translator.translate_to_braille(@file_reader)
   end
 
-  def add_translation
-    File.open(@file_writer, "a") do |f|
-      f.write(@braille_message)
-    end
+  def file_writer
+    file_writer = FileWriter.new(@braille_translation)
+    file_writer.write_new_file
+    file_writer.add_translation
   end
+
 end
 
 nightwriter = NightWriter.new
-puts nightwriter.display_confirmation
-# require "pry";binding.pry
 nightwriter.braille_translate
-nightwriter.add_translation
+nightwriter.file_writer
+puts nightwriter.display_confirmation
