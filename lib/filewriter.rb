174163868b1c924b8798117f2450126d33d183aa
Wyattwicks <wyattwicks@gmail.com>
2021-03-07 23:46:02 -0700

    Revert "Revert "Get iteration 2 to work""
    This reverts commit bac2ceada99b1b89af789bd0e2807020c252b4d8.

--- lib/filewriter.rb
+++ lib/filewriter.rb
@@ -1,9 +1,30 @@
 
 class FileWriter
-   attr_reader :file_write
+   attr_reader :file_writer, :braille_message, :formatted_string
+
+  def initialize(braille_message)
+    @file_writer = ARGV[1]
+    @braille_message = braille_message
+    @formatted_string = add_line_breaks
+  end
+
+  def add_line_breaks
+    string_collection = ""
+    @braille_message.each do |line|
+      string_collection << (line + "\n")
+    end
+    @formatted_string = string_collection.chomp
+  end
 
   def write_new_file
-    File.new(ARGV[1], "w")
+    File.new(@file_writer, "w")
+  end
+
+  def add_translation
+
+    File.open(@file_writer, "a") do |f|
+      f.write(@formatted_string)
+    end
   end
 
 end
--- lib/translator.rb
+++ lib/translator.rb
@@ -8,22 +8,24 @@ class Translator
 
   def initialize
     @braille_message = ["", "", ""]
-    @file_read = file_read
   end
 
-  def translate_to_braille(file_read)
+  def translate_to_braille(file)
+    file_read = file.tr("\n", "")
     character_count = 0
     file_read.each_char do |char|
       add_row if character_count % 40 == 0 && character_count != 0
-        format_braille_character(braille_alphabet[char], character_count)
+        format_braille_character(braille_alphabet[char.downcase], character_count)
         character_count += 1
     end
     @braille_message
+
   end
 
   def format_braille_character(braille_character, character_position)
     line_position = 0
     until line_position > 2 do
+      # require "pry";binding.pry
       @braille_message[determine_array_position(character_position) + line_position] << braille_character[line_position]
       line_position += 1
     end
