99c3a96d882d350d5373c59cf83a585355c6cbeb
Wyattwicks <wyattwicks@gmail.com>
2021-03-08 21:17:51 -0700

    Add refactored night_reader & writer test files

---
+++ test/night_reader_test.rb
@@ -0,0 +1,10 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/night_reader'
+require './data/brailletest.txt'
+require './data/messagetest.txt'
+class NightReaderTest < Minitest::Test
+  def test_it_exists
+    nightreader = NightReader.new
+    assert_instance_of NightReader, nightreader
+  end
